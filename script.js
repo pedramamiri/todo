b12c794a1fab92d7ae9864574268950a7bdf8054
pedramamiri <32764741+pedramamiri@users.noreply.github.com>
2017-11-16 23:02:00 +0100

    login

---
+++ script.js
@@ -0,0 +1,23 @@
+ $(document).ready(function(){
+    sessionStorage.myId = "test";
+    sessionStorage.myPass = "password";
+    sessionStorage.situ;
+    if (sessionStorage.situ !=="in" || sessionStorage.situ == null){
+     $(".klik").click(function(){
+        if (sessionStorage.myId == $(".idname").val() && sessionStorage.myPass == $(".passw").val()) {
+        $(".inlogad").hide();
+        console.log("yesss");
+        sessionStorage.situ = "in"
+
+        
+
+        }
+    
+      
+     });
+    }
+});
+
+
+
+
