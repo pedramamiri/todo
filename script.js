c42fcb63dd569e1b192ad60d983d31fbdf076a2b
pedramamiri <32764741+pedramamiri@users.noreply.github.com>
2017-11-17 02:10:18 +0100

    test2

--- script.js
+++ script.js
@@ -2,11 +2,13 @@
     sessionStorage.myId = "test";
     sessionStorage.myPass = "password";
     sessionStorage.situ;
+    $(".dropdown").hide();
     if (sessionStorage.situ !=="in" || sessionStorage.situ == null){
-     $(".gheyb").click(function(){
+     $(".klik").click(function(){
         if (sessionStorage.myId == $(".idname").val() && sessionStorage.myPass == $(".passw").val()) {
         $(".inlogad").hide();
         console.log("yesss");
+        $(".dropdown").show();
         //sessionStorage.situ = "in"
 
 
